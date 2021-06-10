4a1347ffc9868e910852ee8705f7e0fd324880bd
rosswilsonmedia <82285604+rosswilsonmedia@users.noreply.github.com>
2021-06-09 17:40:09 -0700

    checkerboard commit

---
+++ checkerboard.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/<int:height>')
+def heightAdjust(height):
+    return render_template('index.html', height=height)
+@app.route('/<int:height>/<int:width>')
+def sizeAdjust(height, width):
+    return render_template('index.html', height=height, width=width)
+
+if __name__=='__main__':
+    app.run(debug=True)
